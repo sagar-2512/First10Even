9823124f4266682e0d187b6f4a7cd7810a48d805
Amruta-2000 <collegepc4@gmail.com>
2024-07-31 16:26:57 +0530

    ABCDE Message

---
+++ First10Even.java
@@ -0,0 +1,12 @@
+//write a program to print first 10 even numbers in java.
+
+public class First10Even {
+
+	public static void main(String[] args)
+		{
+			for (int i = 2; i <= 20; i += 2) 
+			{
+		        System.out.println(i);
+		    }
+		}
+}
